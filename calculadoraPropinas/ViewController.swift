298497497dab2709c1de6efe4c29dc8745d25aee
José Trejo <joseatrejos>
2022-08-18 09:37:00 -0700

    Calculadora de propinas

--- calculadoraPropinas/ViewController.swift
+++ calculadoraPropinas/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var txtfDinero: UITextField!
+    @IBOutlet weak var sldPropina: UISlider!
+    @IBOutlet weak var lblPorcentajePropina: UILabel!
+    @IBOutlet weak var lblPropina: UILabel!
+    @IBOutlet weak var lblTotalPropina: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +24,24 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func doChangePropina(_ sender: Any) {
+        
+        let miString = "20.2"
+        let miFloat = Float(miString)
+        
+        lblPorcentajePropina.text = "\(Int(sldPropina.value))%"
+        let cuenta = Float(txtfDinero.text!) ?? 0.0
+        let porcentajePropina = Float(Int(sldPropina.value))
+        let propina = cuenta * porcentajePropina / 100
+        lblPropina.text = String(format: "%.2f", propina)
+        let total = cuenta + propina
+        lblTotalPropina.text = String(format: "%.2f", total)
+        
+    }
+    
+    
+    
 
 
 }
